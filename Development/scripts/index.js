54b48d1272970cc338c7ae32f0a4bb724dafa76d
Salvatore Torcivia <sal@saltor.nyc>
2016-05-14 22:04:43 -0400

    Adjust templates to update via $scope.$digest

--- Development/scripts/index.js
+++ Development/scripts/index.js
@@ -3,79 +3,64 @@ angular.module('runescapeHiscores', ['ngRoute', 'ngAnimate'])
     .controller('mainController', [ '$scope',
         function($scope) {
             $scope.skills = [
-                {name: "attack"},
-                {name: "hitpoints"},
-                {name: "mining"},
-                {name: "strength"},
-                {name: "agility"},
-                {name: "smithing"},
-                {name: "defence"},
-                {name: "herblore"},
-                {name: "fishing"},
-                {name: "ranged"},
-                {name: "thieving"},
-                {name: "cooking"},
-                {name: "prayer"},
-                {name: "crafting"},
-                {name: "firemaking"},
-                {name: "magic"},
-                {name: "fletching"},
-                {name: "woodcutting"},
-                {name: "runecrafting"},
-                {name: "slayer"},
-                {name: "farming"},
-                {name: "construction"},
-                {name: "hunter"}
-            ];
+                {skill: "attack", level: 1},
+                {skill: "hitpoints", level: 1},
+                {skill: "mining", level: 1},
+                {skill: "strength", level: 1},
+                {skill: "agility", level: 1},
+                {skill: "smithing", level: 1},
+                {skill: "defence", level: 1},
+                {skill: "herblore", level: 1},
+                {skill: "fishing", level: 1},
+                {skill: "ranged", level: 1},
+                {skill: "thieving", level: 1},
+                {skill: "cooking", level: 1},
+                {skill: "prayer", level: 1},
+                {skill: "crafting", level: 1},
+                {skill: "firemaking", level: 1},
+                {skill: "magic", level: 1},
+                {skill: "fletching", level: 1},
+                {skill: "woodcutting", level: 1},
+                {skill: "runecrafting", level: 1},
+                {skill: "slayer", level: 1},
+                {skill: "farming", level: 1},
+                {skill: "construction", level: 1},
+                {skill: "hunter", level: 1},
+            ]
+
+            $scope.overall = 0
+            $scope.overall_rank = 0
+
+            $("#form").submit(function(event){
+                event.preventDefault()
+
+                let form = $(this),
+                    user = $("#user-name").val()
+
+                $.get('/player/' + user, appendSkills)
+                    .success(() => form.trigger('reset'))
+                    .fail((error) => console.log('Not sure what happened exactly, but here\s the error report: ', error))
+            })
+
+            function appendSkills(data) {
+                let user = $("#user-name").val()
+
+                console.groupCollapsed(user) // To organize our console logs
+
+                data.map( (index) => console.log(JSON.stringify(index)) )
+
+                let total_combt = []
+
+                let skills  = data.filter( (index) => index.skill !== 'overall' )
+                let overall = data.filter( (index) => index.skill  == 'overall' )
+
+                $scope.skills  = skills;
+                $scope.overall      = overall[0].level
+                $scope.overall_rank = overall[0].rank
+
+                $scope.$digest()
+
+                console.groupEnd()
+            }
         }
     ]);
-
-
-$(document).ready(function() {
-    $("#form").submit(function(event){
-        event.preventDefault()
-
-        let form = $(this),
-            user = $("#user-name").val()
-
-        $.get('/player/' + user, appendSkills)
-            .success(() => form.trigger('reset'))
-            .fail((error) => console.log('Not sure what happened exactly, but here\s the error report: ', error))
-    })
-
-    function appendSkills(data) {
-        let user = $("#user-name").val()
-
-        console.groupCollapsed(user) // To organize our console logs
-
-        data.map( (index) => console.log(JSON.stringify(index)) )
-
-        let skills_list = []
-        let total_combt = []
-
-        let skills  = data.filter( (index) => index.skill !== 'overall' )
-        let overall = data.filter( (index) => index.skill  == 'overall' )
-
-        skills.map(function(index) {
-            let skill      = index.skill,
-                rank       = index.rank,
-                level      = index.level,
-                experience = index.experience
-
-            let skillElement = `<div class="skill skill--${skill}"><img class="skill__image skill_image--${skill}" src="./Assets/images/${skill}.png" alt=""><div class="skill__level">${level}</div></div>`
-
-            skills_list.push(skillElement)
-        })
-
-        overall.map(function(index) {
-            let level        = index.level,
-                totalElement = `overall: ${level}`
-            total_combt.push(totalElement)
-        })
-
-        $('.skills, .overall-and-combat').empty()
-        $('.overall-and-combat').append(total_combt)
-        $('.skills').append(skills_list)
-        console.groupEnd()
-    }
-})
