1e8e1263187ae08127ea77d0c9ec571a92b1d1b9
Salvatore Torcivia <sal@saltor.nyc>
2016-08-29 22:21:14 -0400

    Remove postcss task loading from Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,7 +94,6 @@ module.exports = function(grunt){
 
     grunt.loadNpmTasks('grunt-babel');
     grunt.loadNpmTasks('grunt-notify');
-    grunt.loadNpmTasks('grunt-postcss');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
