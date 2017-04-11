dde1274f1b8574e56ec38822aa4e2a2b5264c6aa
Salvatore Torcivia <sal@saltor.nyc>
2017-04-10 22:34:52 -0400

    Remove id generator, separate stats formatter from server logic and simplify it too

---
+++ source/js/helpers/calc_combat_level.js
@@ -0,0 +1,17 @@
+module.exports = function (stats) {
+    let _ = require("lodash")
+
+    function find(skill) {
+        return _.find(stats, stat => stat.skill === skill)
+    }
+
+    let attack = find("attack"),
+        strength = find("strength"),
+        defence = find("defence"),
+        hitpoints = find("hitpoints"),
+        ranged = find("ranged"),
+        magic = find("magic"),
+        prayer = find("prayer")
+
+    return _.floor(0.25 * (defence.level + hitpoints.level + _.floor(prayer.level / 2)) + _.max([0.325 * (attack.level + strength.level), _.max([0.325 * (_.floor(ranged.level / 2) + ranged.level), 0.325 * (_.floor(magic.level  / 2) + magic.level)])]))
+}
---
+++ source/js/helpers/format_response.js
@@ -0,0 +1,22 @@
+module.exports = function (args) {
+    try {
+        let calc_cb = require("./calc_combat_level.js"),
+            _ = require("lodash")
+
+        let combatFilter = /(attack|strength|defence|hitpoints|magic|ranged|prayer)/i,
+            skill_stats = _.values(args.skills),
+            skill_names = _.keys(args.skills)
+
+        let stats = skill_stats.map((stat, stat_index) => {
+            return _.assign({ id: stat_index, skill: skill_names[stat_index], exp: stat.exp }, stat)
+        })
+
+        let overall = _.reject(stats, { skill: "overall" })
+
+        overall.combat_level = calc_cb(_.filter(stats, index => index.skill.match(combatFilter)))
+
+        return stats
+    } catch(error) {
+        console.log(error)
+    }
+}
