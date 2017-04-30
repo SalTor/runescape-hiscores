759ff090bb56fe6a036e7f2fe66b23b2ecbbe06f
Salvatore Torcivia <sal@saltor.nyc>
2017-04-29 21:30:32 -0400

    Remove unused packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -151,7 +151,6 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-browser-sync');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-postcss');
     grunt.loadNpmTasks('grunt-rsync');
     grunt.loadNpmTasks('grunt-contrib-concat')
